2ab889814906e05d4a958a90414f148912c750dc
sagor009988 <soheltashjid001@gmail.com>
2024-06-09 22:10:46 +0600

    university management system prioject setup complete

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors'
+const app: Application = express();
+
+//parsers
+app.use(express()),
+app.use(cors())
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!wwwwrrr    ');
+});
+
+export default app;
